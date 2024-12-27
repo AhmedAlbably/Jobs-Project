9804be0507ca59712f99d39748ace4356e2a9a23
AhmedAlbably <ahmedalbably777@gmail.com>
2024-12-27 16:02:58 +0200

    show up

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const port = 8080 || process.env.PORT;
 const Job = require("./models/Job");
 const LastJobId = require("./models/LastJobId");
 
